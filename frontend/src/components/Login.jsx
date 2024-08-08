f92b852854935b31e84f0e33d6338d6bd168ed2f
Nathan Johanes Yefta <102526580+yeftanmaa@users.noreply.github.com>
2024-08-08 15:33:26 +0700

    [design] beautify looks on sign in page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -28,13 +28,16 @@ const Login = () => {
             <div className="hero-body">
                 <div className="container">
                     <div className="columns is-centered">
-                        <div className="column is-3-desktop">
+                        <div className="column is-4-desktop">
+                            <div className="has-text-centered">
+                                <img src='/icons/icon_waving-hand.png' width={50} />
+                            </div>
                             <div class="block has-text-centered">
-                                <h1 class="subtitle is-1">Sign in</h1>
+                                <h1 class="subtitle is-1 has-text-black has-text-weight-semibold">Sign in</h1>
                             </div>
 
                             <div className="block has-text-centered">
-                                Welcome back! Please enter your email and <br></br> password before do a booking.
+                                Welcome back! Please enter your email and password before do a booking.
                             </div>
                             <form onSubmit={Auth}>
                                 <p className="has-text-centered">{msg}</p>
@@ -49,9 +52,16 @@ const Login = () => {
                                     <div className="controls">
                                         <input type="password" className="input" placeholder="****************" value={password} onChange={(e) => setPassword(e.target.value)} />
                                     </div>
+                                    <div class="content is-size-7 mt-1">
+                                        <a class="has-text-grey-darker is-size-7 is-underlined" href="#">Forgot Password</a>
+                                    </div>
                                 </div>
                                 <div className="field mt-5">
-                                    <button className="button is-link is-fullwidth">Login</button>
+                                    <button className="button is-link is-fullwidth mb-2">Login</button>
+                                    <div class="content has-text-centered is-size-7">
+                                        Don't have an account?
+                                        <a class="has-text-link is-size-7" href="/register"> Register</a>
+                                    </div>
                                 </div>
                             </form>
                         </div>
